1af1e0c5994c5c9a26bb7389017b6da256283714
Reizora <152237158+Reizora1@users.noreply.github.com>
2023-12-07 22:51:34 +0800

    Update createInvoice.js
    added condition of selectedValue != ""

--- monitoringFiles/createInvoice.js
+++ monitoringFiles/createInvoice.js
@@ -4,24 +4,28 @@ function createInvoice() {
     const mobileNo = document.getElementById('mobileNo').value;
     const eWalletSelected = document.getElementById('selectedEwallet');
     const selectedValue = eWalletSelected.value;
-    const data = { amount, mobileNo, selectedValue };
-
-    fetch('https://apsc-monitoring-webpage.onrender.com/create-invoice', {
+    if(selectedValue != ""){
+        const data = { amount, mobileNo, selectedValue };
+        fetch('http://localhost:3000/create-invoice', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(data),
-    })
-    .then(response => response.json())
-    .then(result => {
-        console.log('Invoice created successfully:', result);
-        alert('Redirecting to checkout url.');
-        console.log(result.invoice_url);
-        window.location.href = result.invoice_url;
-    })
-    .catch(error => {
-        console.error('Error creating invoice:', error);
-        alert('Error creating invoice. Please try again.');
-    });
+        })
+        .then(response => response.json())
+        .then(result => {
+            console.log('Invoice created successfully:', result);
+            alert('Redirecting to checkout url.');
+            console.log(result.invoice_url);
+            window.location.href = result.invoice_url;
+        })
+        .catch(error => {
+            console.error('Error creating invoice:', error);
+            alert('Error creating invoice. Please try again.');
+        });
+    }
+    else{
+        alert('Please select an eWallet for the payment.');
+    }
 }
