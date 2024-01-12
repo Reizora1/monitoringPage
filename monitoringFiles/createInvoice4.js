df3703b542270b7bc472ba62f62b888ea1f03bf0
Reizora <152237158+Reizora1@users.noreply.github.com>
2024-01-12 14:57:29 +0800

    Add files via upload
    added dedicated createInvoice.js scripts for each paymentPage.html

---
+++ monitoringFiles/createInvoice4.js
@@ -0,0 +1,51 @@
+// createInvoice.js
+async function createInvoice() {
+    const apiKey = 'xnd_development_pBpucoqdlThPhCFsJixhcQh0SCVIBnTAS8JNiDPKtJaWbZSEKd78AYwqiPcKs';
+    const amount = document.getElementById('amount').value;
+    const mobileNo = document.getElementById('mobileNo').value;
+    const eWalletSelected = document.getElementById('selectedEwallet');
+    const eWallet = eWalletSelected.value;
+    
+    if(amount == ""){
+        alert('Please input amount for the payment.');
+    }
+    else if(amount < 10){
+        alert('Please input the minimum amount required for the transaction.');
+    }
+    else if(eWallet == ""){
+        alert('Please select an eWallet for the payment.');
+    }
+    else{
+        const payload = {
+            //externalID is the unique identifier for each individual machines.
+            "external_id": "machineTest4",
+            "amount": amount,
+            "description": "Invoice Demo #123",
+            "invoice_duration": 300,
+            "currency": "PHP",
+            "payment_methods": [eWallet],
+            "customer": {
+                "mobile_number": mobileNo
+            },
+        };
+        try {
+            const response = await fetch('https://api.xendit.co/v2/invoices', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Basic ' + window.btoa(apiKey + ':').toString('base64'),
+                },
+                body: JSON.stringify(payload),
+            });
+            const result = await response.json();
+
+            console.log('Invoice created successfully:', result);
+            let alertText = `Redirecting to checkout url.\nTransaction ID: ${result.id}`;
+            alert(alertText);
+            window.location.href = result.invoice_url;
+
+        } catch (error) {
+            console.error('Error creating invoice:', error);
+        }
+    }
+}
